3a325b2ee75ef27136a85bb7047b3c10a62ca208
tewi0314 <tewi0314@gmail.com>
2023-01-03 10:45:14 +0900

    day05

---
+++ day05/src/condition/Test03.java
@@ -0,0 +1,19 @@
+package condition;
+import java.util.Scanner;
+public class Test03 {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		int year = in.nextInt();
+		int month = in.nextInt();
+		
+		boolean y = (year%400==0 || year%100!=0 && year%4 ==0);
+		
+		switch(month){
+			case 1,3,5,7,8,10,12 :  System.out.println("31");
+			case 4,6,9,11 : System.out.println("30");
+			case 2: if(y) System.out.println(29);
+			else System.out.println(28);
+		}
+	}
+}
